aecdb157a2234ed8c09043ef1b166ade1a4a8a97
Askie Lin <askie_lin@trendmicro.com>
2021-03-19 16:38:27 +0800

    fix: using seeds as hook to set initial data

--- src/server.js
+++ src/server.js
@@ -4,6 +4,13 @@ import { createServer, Model } from 'miragejs';
 export default function () {
   createServer({
     models: { reminder: Model },
+
+    seeds(server) {
+      server.create('reminder', { text: 'Walk the dog' });
+      server.create('reminder', { text: 'Take out the trash' });
+      server.create('reminder', { text: 'Work out' });
+    },
+
     routes() {
       this.get('/api/reminders', (schema) => {
         return schema.reminders.all();
