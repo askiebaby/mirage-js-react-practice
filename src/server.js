5bf81270d2e330517a6a56c1b974d9f01c8e8976
Askie Lin <askie_lin@trendmicro.com>
2021-03-19 15:31:53 +0800

    feat: add post route

--- src/server.js
+++ src/server.js
@@ -11,6 +11,19 @@ export default function () {
           { id: 3, text: 'Work out' },
         ],
       }));
+
+      this.post('/api/reminders', (schema, request) => {
+        // 1. request is JSON contains information about request
+        // 2. requestBody is the body of request
+        let attrs = JSON.parse(request.requestBody);
+
+        // open console and add a reminder to the body
+        console.log(attrs);
+
+        debugger;
+
+        // There is no return, so it errors.
+      });
     },
   });
 }
