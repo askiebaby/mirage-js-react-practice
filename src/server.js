36e81ed801119f04aa818ce112d79dd88bc2af62
Askie Lin <askie_lin@trendmicro.com>
2021-03-19 17:02:04 +0800

    feat: using dynamic segment

--- src/server.js
+++ src/server.js
@@ -23,6 +23,12 @@ export default function () {
 
         return schema.reminders.create(attrs);
       });
+
+      this.delete('/api/reminders/:id', (schema, request) => {
+        let id = request.params.id;
+
+        return schema.reminders.find(id).destroy();
+      });
     },
   });
 }
