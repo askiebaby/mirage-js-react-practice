d67e2ebb7008dd09b99bf11ca2ca9811740bdf94
Askie Lin <askie_lin@trendmicro.com>
2021-03-19 15:27:58 +0800

    init mirage tutorial and add get route

---
+++ src/server.js
@@ -0,0 +1,16 @@
+// Welcome to the tutorial!
+import { createServer } from 'miragejs';
+
+export default function () {
+  createServer({
+    routes() {
+      this.get('/api/reminders', () => ({
+        reminders: [
+          { id: 1, text: 'Walk the dog' },
+          { id: 2, text: 'Take out the trash' },
+          { id: 3, text: 'Work out' },
+        ],
+      }));
+    },
+  });
+}
